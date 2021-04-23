043a60d49d98499788e0816d4a9114611455a862
yekesit <yekesit@gmail.com>
2021-04-22 22:37:41 -0700

    Init repo

---
+++ cmake-build-debug/Leetcode/CMakeFiles/L1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L1.dir/main1.cpp.o"
+  "L1"
+  "L1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/Leetcode/CMakeFiles/L2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L2.dir/main2.cpp.o"
+  "L2"
+  "L2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
