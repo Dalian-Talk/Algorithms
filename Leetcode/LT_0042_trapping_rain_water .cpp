c08b3dca36610456c0063a6274216ec7af4272d8
yekesit <yekesit@gmail.com>
2021-06-05 18:22:40 -0700

    LT 42. Trapping Rain Water 275 276从0开始更好理解。。刚开始给自己带歪了。。

--- Leetcode/LT_0042_trapping_rain_water .cpp
+++ Leetcode/LT_0042_trapping_rain_water .cpp
@@ -272,8 +272,8 @@ public:
     int trap(vector<int>& height) {
         int n = height.size();
         if(!n) return 0;
-        int left_max = height[0];
-        int right_max = height[n - 1];
+        int left_max = 0;
+        int right_max = 0;
         int res = 0;
         int i = 0, j = n - 1;
         while (i < j) {
